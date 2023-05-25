8e70a4e52d738cd44260fd4f0bc68519c50db03c
whoibrar <76493188+whoibrar@users.noreply.github.com>
2023-05-25 18:33:59 +0530

    Summary on smaller screen

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -17,7 +17,7 @@ const BlogPost = ({ post }) => {
           </time>
         </header>
         <main>
-          <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+          <p className="md:leading-8 text-gray-700 dark:text-gray-300">
             {post.summary}
           </p>
         </main>
